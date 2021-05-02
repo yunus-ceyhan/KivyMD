48e9b6afdfc30758bbef02ddeab885ee6225275f
yunus-ceyhan <33290609+yunus-ceyhan@users.noreply.github.com>
2021-05-02 20:43:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
                 "sphinx-notfound-page",
             ],
         },
-        install_requires=["kivy>=2.0.0", "pillow"],
+        install_requires=["pillow"],
         setup_requires=[],
         python_requires=">=3.6",
         entry_points={
